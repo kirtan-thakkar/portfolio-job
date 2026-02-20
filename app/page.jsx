96437eecab2f0885958b8e4e772af3397f304143
Kirtan Thakkar <kirtanthakkar30@gmail.com>
2026-02-20 16:14:19 +0530

    Add primary colors for light and dark theme in globals.css

--- app/page.jsx
+++ app/page.jsx
@@ -3,11 +3,18 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="mt-4 min-h-screen tracking-tight">
-      <Container >
-      <h1 className="text-2xl md:text-4xl text-center text-neutral-800">hello There !</h1>
-      <p className="text-neutral-500 text-[20px]">I am a second year comp-sci student. I am a hardworking and dedicated individual with a passion of problem solving. I create bug less web apps that helps bussiness to gain digital presence with credibility. I craft web apps with Nextjs mongodb framer motion, and Gsap</p>
-    </Container>
+    <div className="mt-4  min-h-screen  tracking-tight">
+      <Container className="min-h-screen p-4">
+        <h1 className=" text-2xl md:text-4xl text-neutral-800/95 font-medium tracking-tighter">
+          Hello
+        </h1>
+        <p className="text-neutral-500 text-md ">
+          I am a second year comp-sci student. I am a hardworking and dedicated
+          individual with a passion of problem solving. I create bug less web
+          apps that helps bussiness to gain digital presence with credibility. I
+          craft web apps with Nextjs mongodb framer motion, and Gsap
+        </p>
+      </Container>
     </div>
   );
 }
--- components/Container.jsx
+++ components/Container.jsx
@@ -1,4 +1,4 @@
 const Container = ({ children, className }) => {
-  return <div className={`${className} bg-white dark:bg-black max-w-4xl mx-auto `}>{children}</div>;
-};
+  return <div className={`${className} bg-white dark:bg-black max-w-4xl mx-auto  `}>{children}</div>;
+}; 
 export default Container;
