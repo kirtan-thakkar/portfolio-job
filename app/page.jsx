538febb52e6d1ddb46aed0d26f40b62afa68dfb5
Kirtan Thakkar <kirtanthakkar30@gmail.com>
2026-02-20 16:42:23 +0530

    Addd .prettierrc.json

--- app/page.jsx
+++ app/page.jsx
@@ -3,12 +3,12 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="mt-4  min-h-screen  tracking-tight">
+    <div className="mt-4 min-h-screen tracking-tight">
       <Container className="min-h-screen p-4">
-        <h1 className=" text-2xl md:text-4xl text-primary font-medium tracking-tighter">
+        <h1 className="text-primary text-2xl font-medium tracking-tighter md:text-4xl">
           Hello
         </h1>
-        <p className="text-secondary text-sm md:text-base ">
+        <p className="text-secondary text-sm md:text-base">
           I am a second year comp-sci student. I am a hardworking and dedicated
           individual with a passion of problem solving. I create bug less web
           apps that helps bussiness to gain digital presence with credibility. I
