c3109f2f1041686689ac21b8be9b272a1763858b
Kirtan Thakkar <kirtanthakkar30@gmail.com>
2026-02-20 16:19:43 +0530

    Add tailwind class Pretier

--- app/page.jsx
+++ app/page.jsx
@@ -5,10 +5,10 @@ export default function Home() {
   return (
     <div className="mt-4  min-h-screen  tracking-tight">
       <Container className="min-h-screen p-4">
-        <h1 className=" text-2xl md:text-4xl text-neutral-800/95 font-medium tracking-tighter">
+        <h1 className=" text-2xl md:text-4xl text-primary font-medium tracking-tighter">
           Hello
         </h1>
-        <p className="text-neutral-500 text-md ">
+        <p className="text-secondary text-sm md:text-base ">
           I am a second year comp-sci student. I am a hardworking and dedicated
           individual with a passion of problem solving. I create bug less web
           apps that helps bussiness to gain digital presence with credibility. I
