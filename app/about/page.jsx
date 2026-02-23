f46ad1fe39b40317e7124f1bd110c47a910f49d7
Kirtan Thakkar <kirtanthakkar30@gmail.com>
2026-02-23 18:17:03 +0530

    Add about page

---
+++ app/about/page.jsx
@@ -0,0 +1,22 @@
+import Container from "@/components/Container";
+import Image from "next/image";
+export default function AboutPage() {
+  return (
+    <>
+      <div className="min-h-[200vh] p-10 md:p-10 mb:pt-20 tracking-tight">
+        <Container className="min-h-screen p-4 md:p-10">
+          <h1 className="text-primary text-2xl font-medium tracking-tighter md:text-4xl">
+            About Me
+          </h1>
+          <p className="text-secondary max-w-lg pt-4 text-sm md:text-base">
+            I am a second year comp-sci student. I am a hardworking and
+            dedicated individual with a passion of problem solving. I create bug
+            less web apps that helps bussiness to gain digital presence with
+            credibility. I craft web apps with Nextjs mongodb framer motion, and
+            Gsap
+          </p>
+        </Container>
+      </div>
+    </>
+  );
+}
--- components/project.jsx
+++ components/project.jsx
@@ -1,8 +1,8 @@
-"use client"
+"use client";
 import Image from "next/image";
 import Container from "./Container";
 import Link from "next/link";
-import {motion} from "motion/react";
+import { motion } from "motion/react";
 const Project = () => {
   const completedProject = [
     {
@@ -41,40 +41,43 @@ const Project = () => {
           {completedProject.map((project, index) => {
             return (
               <motion.div
-              initial={{
-                opacity:0,
-                filter:"blur(10px)",
-                y:10
-              }}
-              whileInView={{
-                opacity:1,
-                filter:"blur(0px)",
-                y:0
-              }}
-              transition={{
-                duration:0.3,
-                ease:"easeInOut",
-                delay:index*0.1
-              }}
-               className="group flex flex-col gap-1 md:gap-2" key={index}>
+                initial={{
+                  opacity: 0,
+                  filter: "blur(10px)",
+                  y: 10,
+                }}
+                whileInView={{
+                  opacity: 1,
+                  filter: "blur(0px)",
+                  y: 0,
+                }}
+                transition={{
+                  duration: 0.3,
+                  ease: "easeInOut",
+                  delay: index * 0.1,
+                }}
+                className="group flex flex-col gap-1 md:gap-2 mb-2"
+                key={index}
+              >
                 <Link
                   href={project.url}
                   target="_blank"
                   rel="noopener noreferrer"
+                  className="mb-2"
                 >
                   <Image
                     src={project.image}
                     alt={project.title}
                     width={500}
                     height={500}
-                    className="h-72 w-full rounded-lg object-cover group-hover:blur-[5px] transition-all duration-200 ease-in-out"
+                    className="h-72 w-full rounded-lg object-cover transition-all duration-200 ease-in-out group-hover:blur-[5px]"
                   ></Image>
                 </Link>
                 <div>
-                  <h1 className="text-primary text-sm md:text-base">
+                  <h1 className="text-primary text-sm md:text-base dark:text-neutral-200">
                     {project.title}
                   </h1>
-                  <p className="text-secondary text-xs md:text-sm">
+                  <p className="mt-2 text-secondary text-xs md:text-sm dark:text-neutral-200">
                     {project.description}
                   </p>
                 </div>
