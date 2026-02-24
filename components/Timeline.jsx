c0098725f9d6f9c20715ed2bab0b1fe169bf3796
Kirtan Thakkar <kirtanthakkar30@gmail.com>
2026-02-24 23:12:42 +0530

    Basic Mapped over the data

--- components/Timeline.jsx
+++ components/Timeline.jsx
@@ -7,15 +7,22 @@ export default function TimeLine() {
         "Started learning web development from Apna Collage and created my first web app using HTML, CSS and JavaScript",
     },
     {
-        title:"2025",
-        description:"Started learning React and Nextjs and created my first full stack web app using Nextjs and MongoDB"
+      title: "2025",
+      description:
+        "Started learning React and Nextjs and created my first full stack web app using Nextjs and MongoDB",
     },
-
   ];
   return (
     <>
       <div>
-        
+        {data.map((item, index) => {
+          return (
+            <div key={index}>
+              <p className="text-secondary text-base">{item.title}</p>
+              <p className="text-secondary text-sm">{item.description}</p>
+            </div>
+          );
+        })}
       </div>
     </>
   );
