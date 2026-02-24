6577a61744db7acadc250efe06678ae98df2fd72
Kirtan Thakkar <kirtanthakkar30@gmail.com>
2026-02-24 22:57:09 +0530

    change h-60 of collage to h-50

--- components/collage.jsx
+++ components/collage.jsx
@@ -57,7 +57,7 @@ export function Collage() {
           <img
             src={item.image}
             alt={item.title}
-            className="pointer-events-none relative z-10 h-60 w-80 object-cover" />
+            className="pointer-events-none relative z-10 h-50 w-80 object-cover" />
           <h3
             className="mt-4 text-center text-base font-bold text-neutral-700 dark:text-neutral-300">
             {item.title}
