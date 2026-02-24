309419cd67168e254ab99faa5d51773a8e4eea59
Kirtan Thakkar <kirtanthakkar30@gmail.com>
2026-02-24 23:01:18 +0530

    initialse Timeline component

---
+++ components/Timeline.jsx
@@ -0,0 +1,21 @@
+import Container from "@/components/Container";
+export default function TimeLine() {
+  return (
+    <>
+      <div className="mb:pt-20 min-h-[200vh] p-10 tracking-tight md:p-10">
+        <Container className="min-h-screen p-4 md:p-10">
+          <p className="text-secondary mt-4 max-w-lg pt-4 text-sm md:text-base">
+            I am a second year comp-sci student. I am a hardworking and
+            dedicated individual with a passion of problem solving. I create bug
+            less web apps that helps bussiness to gain digital presence with
+            credibility. I craft web apps with Nextjs mongodb framer motion, and
+            Gsap
+          </p>
+          <p className="text-secondary mt-6 max-w-lg pt-4 text-sm md:text-base">
+            I love to travel.
+          </p>
+        </Container>
+      </div>
+    </>
+  );
+}
