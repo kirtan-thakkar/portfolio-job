76072d8c0bcbb37131904f2a0a4033d11813bbdb
Kirtan Thakkar <kirtanthakkar30@gmail.com>
2026-02-26 00:04:02 +0530

    Initialse motion in about oage

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,3 +1,5 @@
+"use client";
+import { motion } from "motion/react";
 import { Collage } from "@/components/collage";
 import Container from "@/components/Container";
 import TimeLine from "@/components/Timeline";
@@ -6,9 +8,9 @@ export default function AboutPage() {
     <>
       <div className="min-h-[200vh] p-10 md:p-10 mb:pt-20 tracking-tight">
         <Container className="min-h-screen p-4 md:p-10">
-          <h1 className="text-primary text-2xl font-medium tracking-tighter md:text-4xl">
+          <motion.h1 className="text-primary text-2xl font-medium tracking-tighter md:text-4xl">
             About Me
-          </h1>
+          </motion.h1>
           <p className="text-secondary max-w-lg pt-4 text-sm md:text-base">
             I am a second year comp-sci student. I am a hardworking and
             dedicated individual with a passion of problem solving. I create bug
