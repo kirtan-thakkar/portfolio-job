760226c160354cc1831fcfa14768f21eb595d079
Kirtan Thakkar <kirtanthakkar30@gmail.com>
2026-02-25 23:46:59 +0530

    Fix api spell

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -4,7 +4,7 @@ export default function ProjectPage() {
   return (
     <>
       <div className="min-h-[200vh] p-10 md:p-10 mb:pt-20 tracking-tight">
-        <Container className="min-h-screen p-4 md:p-10">
+        <Container className=" p-4 md:p-10">
           <h1 className="text-primary text-2xl font-medium tracking-tighter md:text-4xl">
             projects
           </h1>
