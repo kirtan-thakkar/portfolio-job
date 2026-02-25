2d386b8e428a1bd64747d01100078162e3779c2a
Kirtan Thakkar <kirtanthakkar30@gmail.com>
2026-02-26 00:06:56 +0530

    Add animation to subHeading-about page

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -10,18 +10,22 @@ export default function AboutPage() {
         <Container className="min-h-screen p-4 md:p-10">
           <motion.h1
             initial={{ opacity: 0, y: 5, filter: "blur(10px)" }}
-            whileInView={{ opacity: 1, y: 0, filter: "blur(0px)",delay:0.1 }}
+            whileInView={{ opacity: 1, y: 0, filter: "blur(0px)",delay:0.2 }}
             transition={{ duration: 0.2 }}
             className="text-primary text-2xl font-medium tracking-tighter md:text-4xl">
             About Me
           </motion.h1>
-          <p className="text-secondary max-w-lg pt-4 text-sm md:text-base">
+          <motion.p 
+            initial={{ opacity: 0, y: 5, filter: "blur(10px)" }}
+            whileInView={{ opacity: 1, y: 0, filter: "blur(0px)",delay:0.3 }}
+            transition={{ duration: 0.3 }}
+            className="text-secondary max-w-lg pt-4 text-sm md:text-base">  
             I am a second year comp-sci student. I am a hardworking and
             dedicated individual with a passion of problem solving. I create bug
             less web apps that helps bussiness to gain digital presence with
             credibility. I craft web apps with Nextjs mongodb framer motion, and
             Gsap
-          </p>
+          </motion.p>
           <p className="mt-6 text-secondary max-w-lg pt-4 text-sm md:text-base">
             I love to travel.
           </p>
