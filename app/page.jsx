833f8829659cf825e82eaa63cdd9a4260cba4305
Kirtan Thakkar <kirtanthakkar30@gmail.com>
2026-02-26 00:00:10 +0530

    Add animation to h1 in main page

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,20 @@
+"use client";
 import Container from "@/components/Container";
 import Project from "@/components/project";
-import Image from "next/image";
+import { motion } from "motion/react";
 export default function Home() {
   return (
     <>
-      <div className="min-h-[200vh] p-10 md:p-10 mb:pt-20 tracking-tight">
+      <div className="mb:pt-20 min-h-[200vh] p-10 tracking-tight md:p-10">
         <Container className="min-h-screen p-4 md:p-10">
-          <h1 className="text-primary text-2xl font-medium tracking-tighter md:text-4xl">
+          <motion.h1
+            initial={{ opacity: 0, y: 5, filter: "blur(10px)" }}
+            whileInView={{ opacity: 1, y: 0, filter: "blur(0px)",delay:0.1 }}
+            transition={{ duration: 0.3 }}
+            className="text-primary text-2xl font-medium tracking-tighter md:text-4xl"
+          >
             Hello
-          </h1>
+          </motion.h1>
           <p className="text-secondary max-w-lg pt-4 text-sm md:text-base">
             I am a second year comp-sci student. I am a hardworking and
             dedicated individual with a passion of problem solving. I create bug
