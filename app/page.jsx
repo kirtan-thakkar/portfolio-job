6dd2ea7efb0ba295b00a6106e299107c16bbf70a
Kirtan Thakkar <kirtanthakkar30@gmail.com>
2026-02-26 00:01:57 +0530

    Add motion ti subHeading

--- app/page.jsx
+++ app/page.jsx
@@ -10,18 +10,22 @@ export default function Home() {
           <motion.h1
             initial={{ opacity: 0, y: 5, filter: "blur(10px)" }}
             whileInView={{ opacity: 1, y: 0, filter: "blur(0px)",delay:0.1 }}
-            transition={{ duration: 0.3 }}
+            transition={{ duration: 0.1 }}
             className="text-primary text-2xl font-medium tracking-tighter md:text-4xl"
           >
             Hello
           </motion.h1>
-          <p className="text-secondary max-w-lg pt-4 text-sm md:text-base">
+          <motion.p 
+            initial={{ opacity: 0, y: 5, filter: "blur(10px)" }}
+            whileInView={{ opacity: 1, y: 0, filter: "blur(0px)",delay:0.2 }}
+            transition={{ duration: 0.3 }}
+          className="text-secondary max-w-lg pt-4 text-sm md:text-base">
             I am a second year comp-sci student. I am a hardworking and
             dedicated individual with a passion of problem solving. I create bug
             less web apps that helps bussiness to gain digital presence with
             credibility. I craft web apps with Nextjs mongodb framer motion, and
             Gsap
-          </p>
+          </motion.p>
           <Project />
         </Container>
       </div>
