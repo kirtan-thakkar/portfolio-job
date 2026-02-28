6f1c484702c3067c0668cb5af9272abd912526c0
Kirtan thakkar <kirtanthakkar30@gmail.com>
2026-02-28 22:59:16 +0530

    Fix layouting

--- components/ContactForm.jsx
+++ components/ContactForm.jsx
@@ -1,16 +1,23 @@
+"use client";
+import{useState} from "react";
 const ContactForm =()=>{
     const handleSubmit =(event)=>{
         event.preventDefault();
         console.log("Form Submitted");
         
     }
+    const [formData,setFormData]=useState({
+        name:"",
+        email:"",
+        message:""
+    });
     return(
-        <div className="py-10">
+        <div className="py-10"> 
             <form onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="name">Enter Your Name</label>
+                <div className="flex flex-col gap-2">
+                    <label htmlFor="name">Full Name</label>
                     <input id="name" type="text" placeholder="Enter your name" className="border border-gray-300 rounded-md p-2 w-full"></input>
-                    <button className="bg-blue-500 px-4 py-1 cursor-pointer text-white rounded-md">Submit</button>
+                    <button className="bg-blue-500 px-4 py-1 cursor-pointer max-w-sm text-white rounded-md">Submit</button>
                 </div>
             </form>
         </div>
