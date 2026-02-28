5054e5a37791358bb7eac926ae696ab21a2b4733
Kirtan thakkar <kirtanthakkar30@gmail.com>
2026-02-28 22:36:45 +0530

    Initialse handleSubmit handler

--- components/ContactForm.jsx
+++ components/ContactForm.jsx
@@ -1,10 +1,16 @@
 const ContactForm =()=>{
+    const handleSubmit =(event)=>{
+        event.preventDefault();
+        console.log("Form Submitted");
+        
+    }
     return(
         <div className="py-10">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="name">Enter Your Name</label>
                     <input id="name" type="text" placeholder="Enter your name" className="border border-gray-300 rounded-md p-2 w-full"></input>
+                    <button className="bg-blue-500 px-4 py-1 cursor-pointer text-white rounded-md">Submit</button>
                 </div>
             </form>
         </div>
