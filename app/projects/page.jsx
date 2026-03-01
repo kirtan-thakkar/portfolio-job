5dcd94f4fb804dafab72147c05eed62394ff38d3
Kirtan thakkar <kirtanthakkar30@gmail.com>
2026-03-02 00:18:16 +0530

    Update project.jsx,Initialse Motion

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,3 +1,5 @@
+"use client";
+import {motion} from "motion/react";
 import Footer from "@/components/Footer";
 import Container from "@/components/Container";
 export default function ProjectPage() {
@@ -5,12 +7,23 @@ export default function ProjectPage() {
     <>
       <div className="min-h-[200vh] p-10 md:p-10 mb:pt-20 tracking-tight">
         <Container className=" p-4 md:p-10">
-          <h1 className="text-primary text-2xl font-medium tracking-tighter md:text-4xl">
-            projects
-          </h1>
-          <p className="mt-6 text-secondary max-w-lg pt-4 text-sm md:text-base">
-            I love to travel.
-          </p>
+          <motion.h1
+            initial={{ opacity: 0, y: 5, filter: "blur(10px)" }}
+            animate={{ opacity: 1, y: 0, filter: "blur(0px)",delay:0.1 }}
+            transition={{ duration: 0.1 }}
+            className="text-primary text-2xl font-medium tracking-tighter md:text-4xl"
+          >
+            Projects
+          </motion.h1>
+          <motion.p 
+            initial={{ opacity: 0, y: 5, filter: "blur(10px)" }}
+            animate={{ opacity: 1, y: 0, filter: "blur(0px)",delay:0.2 }}
+            transition={{ duration: 0.3 }}
+          className="text-secondary max-w-lg pt-4 text-sm md:text-base">
+           I'm a passionate software engineer dediccated to crafting elegant solutions for complex problems. With expertise in full-stack development, I enjoy building 
+           user-centeric applications that make a difference.
+          </motion.p>
+          
           <Footer />
         </Container>
       </div>
